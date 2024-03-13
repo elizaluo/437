7bc13a1bfebfbc396eceb94031021d97a71ad357
elizaluo <54221405+elizaluo@users.noreply.github.com>
2024-03-13 10:44:32 -0700

    dist frontend pt 2

--- packages/lit-frontend/vite.config.ts
+++ packages/lit-frontend/vite.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: '',
     base: '/',
+    
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                sample_ingredient: 'app/sample_ingredient.html',
+                sample_supermarket: 'app/sample_supermarket.html',
+            },
+        },
     }
 });
