29109dc9112051bae7a31c935ece3f991c53069d
elizaluo <54221405+elizaluo@users.noreply.github.com>
2024-03-13 10:46:50 -0700

    dist frontend pt3

--- packages/lit-frontend/vite.config.ts
+++ packages/lit-frontend/vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
                 main: 'index.html',
                 sample_ingredient: 'app/sample_ingredient.html',
                 sample_supermarket: 'app/sample_supermarket.html',
+                fridge: 'app/fridge.html',
+                groceries: 'app/groceries.html',
+                pantry: 'app/pantry.html',
+                recipes: 'app/recipes.html',
+                sample_recipe: 'app/sample_recipe.html',
+                profile: 'app/profile/profile.html',
             },
         },
     }
